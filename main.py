7eb2da86596c68dfe581c4c5fbdff10a7924c30e
Deagozn <124474840+Deagozn@users.noreply.github.com>
2023-03-14 17:25:46 +0800

    tested (compre working)
    rekog not working

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ s3 = boto3.client('s3')
 
 tempFolder = 'm1tmp/'
 
-IbucketName = "" + awsRegion
+IbucketName = "bucktest5354-" + awsRegion
 
 def drawBoundingBoxes (sourceImage, boxes):
     # blue, green, red, grey
@@ -70,6 +70,9 @@ def rekog(Dimage,detect): # image to be in STR format ; detect to be in LIST for
         }
     }
     )
+
+    display(detectLabelsResponse)
+
     for label in detectLabelsResponse["Labels"]:
         if(label["Name"] in detect):
             print("Detected object:")
@@ -86,67 +89,72 @@ def rekog(Dimage,detect): # image to be in STR format ; detect to be in LIST for
 def compre(Ctext, identifier):  # Ctext is the text to be analyzed ; identifier is the tyoe of data to be analyzed
     if identifier.lower() == "named entities":
         detected_entities = comprehend.detect_entities(Text = Ctext, LanguageCode = 'en')
-        detectec_entities_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_entities['Entities']],
+        detected_entities_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_entities['Entities']],
                 columns=['Text', 'Type', 'Score'])
         print("This was the text analyzed:")
+        print()
         print(Ctext)
         print()
-        display(detectec_entities_df)
+        display(detected_entities_df)
 
     elif identifier.lower() == "key phrases":
         detected_key_phrases = comprehend.detect_key_phrases(Text = Ctext, LanguageCode = 'en')
-        detectec_key_phrases_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_key_phrases['Entities']],
-                columns=['Text', 'Type', 'Score'])
+        detected_key_phrases_df = pd.DataFrame([ [entity['Text'], entity['Score']] for entity in detected_key_phrases['KeyPhrases']],
+                columns=['Text', 'Score'])
         print("This was the text analyzed:")
+        print()
         print(Ctext)
         print()
-        display(detectec_key_phrases_df)
+        display(detected_key_phrases_df)
     
     elif identifier.lower() == "dominant language":
-        detected_language = comprehend.detect_dominant_language(Text = Ctext, LanguageCode = 'en')
-        detectec_language_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_language['Entities']],
-                columns=['Text', 'Type', 'Score'])
+        detected_language = comprehend.detect_dominant_language(Text = Ctext)
+        detected_language_df = pd.DataFrame([ [code['LanguageCode'], code['Score']] for code in detected_language['Languages']],
+                columns=['Language Code', 'Score'])
         print("This was the text analyzed:")
+        print()
         print(Ctext)
         print()
-        display(detectec_language_df)
+        display(detected_language_df)
 
     elif identifier.lower() == "emotional sentiment":
         detected_sentiment = comprehend.detect_sentiment(Text = Ctext, LanguageCode = 'en')
         predominant_sentiment = detected_sentiment['Sentiment']
-        detectec_sentiment_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_sentiment['Entities']],
-                columns=['Text', 'Type', 'Score'])
+        detected_sentiments_df = pd.DataFrame([ [sentiment, detected_sentiment['SentimentScore'][sentiment]] for sentiment in detected_sentiment['SentimentScore']],
+                columns=['Language Code', 'Score'])
         print("This was the text analyzed:")
         print(Ctext)
         print()
         print("The predominant sentiment is {}.".format(predominant_sentiment))
         print()
-        display(detectec_sentiment_df)
+        display(detected_sentiments_df)
 
     elif identifier.lower() == "syntax":
         detected_syntax = comprehend.detect_syntax(Text = Ctext, LanguageCode = 'en')
-        detectec_syntax_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_syntax['Entities']],
-                columns=['Text', 'Type', 'Score'])
+        detected_syntax_df = pd.DataFrame([ [part['Text'], part['PartOfSpeech']['Tag'], part['PartOfSpeech']['Score']] for part in detected_syntax['SyntaxTokens']],
+                columns=['Text', 'Part Of Speech', 'Score'])
         print("This was the text analyzed:")
+        print()
         print(Ctext)
         print()
-        display(detectec_syntax_df)
+        display(detected_syntax_df)
     
     elif identifier.lower() == "pii":
         detected_pii_entities = comprehend.detect_pii_entities(Text = Ctext, LanguageCode = 'en')
-        detectec_pii_entities_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_pii_entities['Entities']],
-                columns=['Text', 'Type', 'Score'])
+        detected_pii_entities_df = pd.DataFrame([ [entity['Type'], entity['Score']] for entity in detected_pii_entities['Entities']],
+                columns=['Type', 'Score'])
         print("This was the text analyzed:")
+        print()
         print(Ctext)
         print()
-        display(detectec_pii_entities_df)
+        display(detected_pii_entities_df)
     
     elif identifier.lower() == "pii labels":
         detected_pii_labels = comprehend.contains_pii_entities(Text = Ctext, LanguageCode = 'en')
-        detectec_pii_labels_df = pd.DataFrame([ [entity['Text'], entity['Type'], entity['Score']] for entity in detected_pii_labels['Entities']],
-                columns=['Text', 'Type', 'Score'])
+        detected_pii_labels_df = pd.DataFrame([ [entity['Name'], entity['Score']] for entity in detected_pii_labels['Labels']],
+                columns=['Name', 'Score'])
         print("This was the text analyzed:")
+        print()
         print(Ctext)
         print()
-        display(detectec_pii_labels_df)
-
+        display(detected_pii_labels_df)
